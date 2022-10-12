b393455ad696b01bbe420ec394a0ba2967674b1c
劉展宏 <>
2022-10-13 00:14:56 +0800

    no message

---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+ 
+    
+    <nav>
+        <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/users">Users</Link></li>
+            <li><Link href="/settings">Setting</Link></li>
+            <li><Link href="/logout" method="post" as="button">Lougot</Link></li>
+        </ul>
+    </nav>
+</template>
+    
+    
+    
+    <script>
+    import {Link} from '@inertiajs/inertia-vue3';
+    
+    
+    export default {
+        components: {Link},
+    };
+    
+    </script>
