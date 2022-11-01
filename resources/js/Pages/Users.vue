5b707175a5fe4014eff352e7855cb7c3a70c2459
劉展宏 <>
2022-11-01 21:50:56 +0800

    no message

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -18,7 +18,7 @@
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
             <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="user in users" :key="user.id">
+              <tr v-for="user in users.data" :key="user.id">
                 <td class="px-6 py-4 whitespace-nowrap">
                   <div class="flex items-center">
                     
@@ -50,6 +50,6 @@
     
   
     <script setup>
-    defineProps({ users:Array});
+    defineProps({ users: Object});
     </script>
     
