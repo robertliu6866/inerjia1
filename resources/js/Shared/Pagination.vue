5dcb86496c5f2637fb07a4ef5f6bbeedbc9898c2
劉展宏 <>
2022-11-04 03:28:02 +0800

    no message

---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,23 @@
+
+<template>
+<div>
+    <Component
+   :is="link.url ? 'Link' : 'span' "
+   v-for="link in users.links"
+   :href="link.url" 
+   v-html="link.label" 
+   :class="link.url ? '' : 'text-gray-500' "
+
+   />
+</div>
+
+</template>
+
+<script>
+export default{
+    props:{
+        links: Array
+
+    }
+};
+</script>
